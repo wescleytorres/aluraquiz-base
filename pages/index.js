36f5f1d978c97811fcc8706d721e83d29b74629e
Wescley Torres <jewescley@gmail.com>
2021-01-25 17:04:22 -0300

    codigo ao final da primeira aula IRN

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,61 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+
+import db from '../db.json'
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+
+            <p>lorem ipsum dolor sit amet...</p>
+
+          </Widget.Content>
+
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner />
+    </QuizBackground>
+  )
 }
